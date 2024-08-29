680087b0521637dd44f5841a441eb2d06f8a9018
Avishka madhuranga <131765154+AvishkaGit@users.noreply.github.com>
2024-08-29 13:23:37 +0530

    player complete
    Player.jsx complete front end

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -17,7 +17,7 @@ const Sidebar = () => {
             </div>
 
             <div className='bg-[#121212] h-[85%] rounded' >
-                <div className='p-4 flex items-center justify-between'>
+                <div className='flex items-center justify-between p-4'>
                     <div className='flex items-center gap-3'>
                         <img className='w-8' src={assets.stack_icon} alt="" />
                         <p className='font-semibold'>Your Library</p>
