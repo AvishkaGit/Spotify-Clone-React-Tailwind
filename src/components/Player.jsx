b428fe7cfa25bd013fda4aa6d7ebfc136cf85f96
Avishka madhuranga <131765154+AvishkaGit@users.noreply.github.com>
2024-08-29 16:05:04 +0530

    disply.jsx
    create songalbum.jsx and SongItems.jsx

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -3,7 +3,7 @@ import { assets, songsData } from '../assets/assets'
 
 const Player = () => {
     return (
-        <div className='h-[10%] bg-black flex justify-between items-center text-white px-4'>
+        <div className='h-[10%] bg-black flex justify-between items-center text-white px-4 '>
             <div className='items-center hidden gap-4 lg:flex'>
                 <img className='w-12' src={songsData[0].image} alt="" />
                 <div>
